05d7011d7e6a1a1eafac0d5561e38af9a2b8f6de
Amit Roy <2017-3-60-021@std.ewubd.edu>
2021-07-24 14:42:06 +0600

    Initial commit

---
+++ Orient Express.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void function(x)
+{
+    int num,i,j,un,count=0;
+    for( ; ; )
+    {
+        printf("Enter Room Number: ");
+        scanf("%d",&num);
+        printf("\n");
+        if(num!=0)
+        {
+            if(num<=100)
+            {
+                for(i=2; i<=10000; i++)
+                {
+                    if(count!=num)
+                    {
+                        for(j=2; j<=i; j++)
+                        {
+                            if(i==j)
+                            {
+                                count=count+1;
+                                un=i;
+                            }
+                            else if(i%j==0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                count=0;
+                printf("Unlock with: %d",un);
+                printf("\t\t\tLock with: %d\n",num+un);
+                printf("\n");
+            }
+            else
+            {
+                printf("Room does not exist.\n");
+                printf("\n");
+            }
+        }
+        else
+            break ;
+
+    }
+}
+
+
+int main()
+{
+    int input;
+    printf("\t\t\tMURDER ON THE ORIENT EXPRESS\n");
+    printf("----------------------------------------------------------------------------\n");
+    function(input);
+return 0;
+}
